437301a4a5c41928b651fb037346c9706bdd62da
90stm <ugonzalez@live.com>
2023-08-02 08:51:42 +0700

    update app.py
    removed previous configurations and then added @app.errorhandler so we can find out why our login and registeration page are not working after hitting submit

--- app.py
+++ app.py
@@ -377,66 +377,10 @@ def submit_feedback():
     return render_template('form.html', feedback_message='Thank you for your feedback!')
 
 
-# ----------------------------
-# Application Factory Function
-# ----------------------------
-
-def create_app(config_filename=None):
-    # Create the Flask application
-    app = Flask(__name__)
-
-    # Configure the Flask application
-    config_type = os.getenv('CONFIG_TYPE', default='config.DevelopmentConfig')
-    app.config.from_object(config_type)
-
-    initialize_extensions(app)
-    register_blueprints(app)
-    configure_logging(app)
-    register_cli_commands(app)
-
-    # Check if the database needs to be initialized
-    engine = sa.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
-    inspector = sa.inspect(engine)
-    if not inspector.has_table("users"):
-        with app.app_context():
-            db.drop_all()
-            db.create_all()
-            app.logger.info('Initialized the database!')
-    else:
-        app.logger.info('Database already contains the users table.')
-
-    return app
-
-
-
-def configure_logging(app):
-    # Logging Configuration
-    if app.config['LOG_WITH_GUNICORN']:
-        gunicorn_error_logger = logging.getLogger('gunicorn.error')
-        app.logger.handlers.extend(gunicorn_error_logger.handlers)
-        app.logger.setLevel(logging.DEBUG)
-    else:
-        file_handler = RotatingFileHandler('instance/flask-user-management.log',
-                                           maxBytes=16384,
-                                           backupCount=20)
-        file_formatter = logging.Formatter('%(asctime)s %(levelname)s %(threadName)s-%(thread)d: %(message)s [in %(filename)s:%(lineno)d]')
-        file_handler.setFormatter(file_formatter)
-        file_handler.setLevel(logging.INFO)
-        app.logger.addHandler(file_handler)
-
-    # Remove the default logger configured by Flask
-    app.logger.removeHandler(default_handler)
-
-    app.logger.info('Starting the Flask User Management App...')
-
-
-def register_cli_commands(app):
-    @app.cli.command('init_db')
-    def initialize_database():
-        """Initialize the database."""
-        db.drop_all()
-        db.create_all()
-        echo('Initialized the database!')
+@app.errorhandler(500)
+def internal_server_error(error):
+    app.logger.error('Server Error: %s', (error))
+    return "500 error", 500
 
 
 if __name__ == '__main__':

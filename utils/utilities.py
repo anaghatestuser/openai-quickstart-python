452923ed7273c562873fe2ddd8fb3686e6283665
90stm <ugonzalez@live.com>
2023-08-10 11:26:27 +0700

    Update utilities.py

--- utils/utilities.py
+++ utils/utilities.py
@@ -4,9 +4,9 @@ import openai
 import logging
 from config import db  # Import the db object from your config module
 
-# nltk.download('punkt') # Add this line to download the 'punkt' resource
+nltk.download('punkt') # Add this line to download the 'punkt' resource
 
-nltk.data.path.append('/opt/render/project/src/nltk_data') # Adjust this path based on where you place the data in your project structure
+# nltk.data.path.append('/opt/render/project/src/nltk_data') # Adjust this path based on where you place the data in your project structure
 
 logger = logging.getLogger(__name__)
 

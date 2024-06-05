db9a69a12cf926675778293582a1830beac00fab
Ulysses <ugonzalez@live.com>
2024-06-05 13:53:15 +0700

    Add methods to generate and verify reset tokens for user passwords.

--- models/user.py
+++ models/user.py
@@ -1,6 +1,8 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime, timedelta
 from flask_login import UserMixin
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import current_app
 from config import db
 
 class User(UserMixin, db.Model):
@@ -26,5 +28,16 @@ class User(UserMixin, db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
-    def __repr__(self):
+    def get_reset_token(self, expires_sec=1800):
+        s = Serializer(current_app.config['SECRET_KEY'], expires_sec)
+        return s.dumps({'user_id': self.id}).decode('utf-8')
+
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return User.query.get(user_id)
         return '<User {}>'.format(self.username)
